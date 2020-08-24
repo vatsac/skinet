d883306dae94be960d1c7723ed35a16f9f3e344b
Aditya Kumar <avats218@gmail.com>
2020-08-24 18:17:41 +0530

    End Of Section 8

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { IProduct } from './models/product';
+import { IPagination } from './models/pagination';
+
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'SkiNet';
+  products: IProduct[];
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe(
+      (response: IPagination) => {
+        this.products = response.data;
+      }, error => {
+        console.log(error);
+      });
+  }
 }
